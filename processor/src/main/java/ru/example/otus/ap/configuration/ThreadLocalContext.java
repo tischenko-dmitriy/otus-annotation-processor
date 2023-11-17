448a8ad055f327d3ebddff0be20dc1992142581f
dmitriy-tischenko <tischenko.prg@gmail.com>
2023-11-18 01:24:25 +0300

    20231117 working version

--- processor/src/main/java/ru/example/otus/ap/configuration/ThreadLocalContext.java
+++ processor/src/main/java/ru/example/otus/ap/configuration/ThreadLocalContext.java
@@ -7,8 +7,6 @@ public final class ThreadLocalContext {
 
     private static final ThreadLocal<RoundEnvironment> ROUND_ENVIRONMENT = new ThreadLocal<>();
     private static final ThreadLocal<ProcessingEnvironment> PROCESS_ENV = new ThreadLocal<>();
-    private static final ThreadLocal<String> PROCESSING_CLASS_NAME = new ThreadLocal<>();
-    private static final ThreadLocal<String> SHOW_VALUES_CONTENT = new ThreadLocal<>();
 
     public static RoundEnvironment getRoundEnvironment() {
         return ROUND_ENVIRONMENT.get();
@@ -25,26 +23,10 @@ public final class ThreadLocalContext {
     public static void setProcessEnv(ProcessingEnvironment processEnv) {
         PROCESS_ENV.set(processEnv);
     }
-    public static String getProcessingClassName() {
-        return PROCESSING_CLASS_NAME.get();
-    }
-
-    public static void setProcessingClassName(String className) {
-        PROCESSING_CLASS_NAME.set(className);
-    }
-
-    public static String getShowValuesContent() {
-        return SHOW_VALUES_CONTENT.get();
-    }
-
-    public static void setShowValuesContent(String toStringContent) {
-        SHOW_VALUES_CONTENT.set(toStringContent);
-    }
 
     public static void remove() {
         ROUND_ENVIRONMENT.remove();
-        PROCESSING_CLASS_NAME.remove();
-        SHOW_VALUES_CONTENT.remove();
+        PROCESS_ENV.remove();
     }
 
 }
